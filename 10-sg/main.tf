560664ae64d9459310318d04d8dc73879205e1e6
Hussain90basha <bashahussain9010@gmail.com>
2025-11-26 17:37:42 +0530

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -16,6 +16,15 @@ module "sg" {
     environment = var.environment
     sg_name = var.sg_names[count.index]
     description = "created for ${var.sg_names[count.index]}"
-    vpc_id = local.vpc_id
-  
+    vpc_id = local.vpc_id  
 }
+
+# # frontend accepting the traffic from fronted_alb
+# resource "aws_security_group_rule" "frontend_frontend_alb" {
+#     type = "ingress"
+#     security_group_id = module.sg[9].sg_id  # frontend SG ID
+#     source_security_group_id =  module.sg[11].sg_id #frontend ALB SG ID
+#     from_port         = 80
+#     protocol       = "tcp"
+#     to_port           = 80
+# }
